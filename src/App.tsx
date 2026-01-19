a384ddc575f5006160b5fca56f33251a6a2307d1
molnia_grom <mgrom250@gmail.com>
2026-01-19 18:38:52 +0200

    have autoscroll, but need to optimization - rerender on almost everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {io, Socket} from "socket.io-client"
 
 // Типизируем объекты для большей надежности
@@ -23,9 +23,12 @@ const socket: Socket = io("http://localhost:3001");
 // const socket: Socket = io("https://my-socket-chat-training-server.onrender.com");
 
 function App() {
+    console.log("app")
     const [messages, setMessages] = useState<MessageType[]>([]);
     const [name, setName] = useState("Alina");
     const [newMessage, setNewMessage] = useState('');
+    const [isAutoScrollActive, setIsAutoScrollActive] = useState(true);
+    const [lastScrollTop, setLastScrollTop] = useState(0);
 
     useEffect(() => {
 
@@ -50,6 +53,12 @@ function App() {
         }
     }, []);
 
+    useEffect(() => {
+        if (isAutoScrollActive) {
+            messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'});
+        }
+    }, [messages, isAutoScrollActive]);
+
     const sendMessage = () => {
         if (newMessage.trim() !== '') {
 
@@ -59,12 +68,27 @@ function App() {
         } else {
             console.log('Empty message, not sending');
         }
-    };
+    }
+
+    const messagesAnchorRef = useRef<HTMLDivElement>(null)
 
     return (
         <div className="App">
             <div className={"container"}>
-                <div className="messages-container">
+                <div className="messages-container" onScroll={(e) => {
+
+                    const element = e.currentTarget;
+                    const maxScrollPosition = element.scrollHeight - element.clientHeight
+                    console.log(maxScrollPosition)
+
+                    if (element.scrollTop > lastScrollTop && Math.abs(maxScrollPosition - element.scrollTop) < 10) {
+                        setIsAutoScrollActive(true)
+                    } else {
+                        setIsAutoScrollActive(false)
+                    }
+                    setLastScrollTop(element.scrollTop)
+
+                }}>
                     {messages.length === 0 && <div>No messages yet...</div>}
                     {messages.map(m => {
                         return (
@@ -74,6 +98,7 @@ function App() {
                             </div>
                         )
                     })}
+                    <div ref={messagesAnchorRef}></div>
                 </div>
                 <br/>
                 <div className={"inputButton"}>
